0beddcfee869a84d14b31121a17fc8ccc89da6b8
mdraihan51674 <mdraihan51674@gmail.com>
2025-07-21 19:25:41 +0600

    added live url

--- src/Hooks/AxiosSeure/useAxiosSecure.jsx
+++ src/Hooks/AxiosSeure/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import useAuth from './useAuth';
 
 // Create an axios instance
 const axiosSecure = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://server-two-ashen.vercel.app',
 });
 
 
